4e54f8ce2c1aaa0095e35b15edd49d89a7a0f69d
Fiju <faixanshirax@gmail.com>
2020-01-20 06:27:50 +0500

    feat:ui updates

---
+++ src/components/AddQuestion/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { requestSaveQuestion } from "../../actions/questionActions";
+
+import styles from "./AddQuestion.module.scss";
+
+const mapStateToProps = state => ({
+  author: state.users.loggedInUser
+});
+
+const mapDispatchToProps = {
+  requestSaveQuestion
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(({ author, requestSaveQuestion, history }) => {
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+  return (
+    <div className={styles.container}>
+      <header>Would You Rather</header>
+      <form>
+        <fieldset>
+          <input
+            value={optionOne}
+            onChange={e => setOptionOne(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            value={optionTwo}
+            onChange={e => setOptionTwo(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            onClick={() => {
+              requestSaveQuestion({
+                author,
+                optionOneText: optionOne,
+                optionTwoText: optionTwo
+              });
+              history.push("/home");
+            }}
+            readOnly
+            className={styles.primaryButton}
+            disabled={!Boolean(optionOne && optionTwo)}
+            value="Submit"
+          />
+        </fieldset>
+      </form>
+    </div>
+  );
+});
---
+++ src/components/LeaderBoard/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import withUsers from "../../containers/withUsers";
+
+import styles from "./LeaderBoard.module.scss";
+
+export default compose(
+  withUsers,
+  connect()
+)(({ users }) => {
+  const leaderBoardData = users
+    .map(({ name, answers, questions, avatarURL }) => ({
+      name,
+      answers: Object.keys(answers).length,
+      questions: Object.keys(questions).length,
+      avatarURL
+    }))
+    .sort((a, b) => b.answers + b.questions - (a.answers + a.questions));
+  return (
+    <div className={styles.container}>
+      <ul>
+        {leaderBoardData.map(({ name, answers, questions, avatarURL }) => (
+          <li className={styles.item}>
+            <div className={styles.nestedContainer}>
+              <img src={avatarURL} alt="" />
+              <div className={styles.score}>
+                <div>{name}</div>
+                <div>
+                  <span>Questions answered: {answers}</span>
+                  <span>Questions asked: {questions}</span>
+                  <span>Total score: {questions + answers}</span>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
